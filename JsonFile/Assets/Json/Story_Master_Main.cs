36b85b40a96a10ac9bb38ee50b75f8ab03753bc0
SoyCkicken <rlaxorud1231@naver.com>
2025-06-13 19:32:09 +0900

    수정 했음

--- JsonFile/Assets/Json/Story_Master_Main.cs
+++ JsonFile/Assets/Json/Story_Master_Main.cs
@@ -8,13 +8,13 @@ public class Story_Master_Main
     public int Event_Index;
     public int Script_Index;
     public string Scene_Code;
-    public bool Next_Scene;
+    public string Next_Scene;
     public string Script_Text;
     public bool Main_Effect;
     public string Choice1_Text;
-    public bool Choice1_Next_Scene;
+    public string Choice1_Next_Scene;
     public string Choice2_Text;
-    public bool Choice2_Next_Scene;
+    public string Choice2_Next_Scene;
     public string Choice3_Text;
     public bool Choice3_Next_Scene;
 }
