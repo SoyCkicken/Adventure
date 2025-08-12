62ffbbc1ce26681a3d9c6ca120b647c8212b9a37
SoyCkicken <rlaxorud1231@naver.com>
2025-08-13 00:40:34 +0900

    GPGS 로그인 테스트
    성공 했음 이제 구글 애드 몹 붙여 봐야함

---
+++ JsonFile/Assets/GoogleManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using TMPro;
+
+public class GoogleManager : MonoBehaviour
+{
+    public TextMeshProUGUI logText;
+
+    void Start()
+    {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        SignIn();
+    }
+
+    public void SignIn()
+    {
+        //PlayGamesPlatform.Instance.Authenticate((SignInStatus success) =>
+        //{
+        //    if (success == SignInStatus.Success)
+        //    {
+        //        string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+        //        string id = PlayGamesPlatform.Instance.GetUserId();
+        //        string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+
+        //        logText.text = "로그인 성공: " + name;
+        //        Debug.Log($"[GPGS] 이름: {name}, ID: {id}, 이미지URL: {ImgUrl}");
+        //    }
+        //    else
+        //    {
+        //        logText.text = "로그인 실패";
+        //        Debug.LogError("[GPGS] 로그인 실패");
+        //    }
+        //});
+
+        PlayGamesPlatform.Instance.Authenticate((SignInStatus result) =>
+        {
+            Debug.LogError($"[GPGS] 로그인 결과: {result}");
+
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+                    string id = PlayGamesPlatform.Instance.GetUserId();
+                    string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+
+                    logText.text = "로그인 성공: " + name;
+                    Debug.Log($"[GPGS] 이름: {name}, ID: {id}, 이미지URL: {ImgUrl}");
+                    //Debug.LogError("개발자 오류 - OAuth 클라이언트 ID가 잘못되었거나 SHA-1이 안 맞음");
+                    break;
+                case SignInStatus.InternalError:
+                    Debug.LogError("GPGS 내부 오류");
+                    break;
+                default:
+                    Debug.LogError($"정의되지 않은 에러: {result}");
+                    break;
+            }
+        });
+    }
+
+
+    internal void ProcessAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            // Continue with Play Games Services
+            // Perfectly login success
+
+            string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+            string id = PlayGamesPlatform.Instance.GetUserId();
+            string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+
+            logText.text = "성공입니다 \n" + name;
+        }
+        else
+        {
+            logText.text = "Sign in Failed!";
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            //PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+            // Login failed
+        }
+    }
+}
